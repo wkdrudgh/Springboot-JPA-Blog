aed776a64d719450bf4bb7c02df023873d98007d
wkdrudgh <boo04178@naver.com>
2020-12-07 00:11:36 +0900

    select 및 paging 테스트

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,19 +1,32 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder 
 @Entity
 public class Board {
 
@@ -30,9 +43,12 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 	
-	@ManyToOne // Many = Board, User = One
+	@ManyToOne(fetch = FetchType.EAGER) // Many = Board, User = One // 검색 할 때 정보를 들고 와라 기본이 EAGER // LAZY는 요청을 보내면 정보를 보내라
 	@JoinColumn(name = "userId")
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 컬럼을 만들지 마세요.
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
