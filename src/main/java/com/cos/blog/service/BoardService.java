44de52e342c5d35155108d27cf5ddcb4ff82513f
wkdrudgh <boo04178@naver.com>
2020-12-16 16:41:29 +0900

    글 수정하기 기능 구현

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,12 +1,8 @@
 package com.cos.blog.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,5 +38,16 @@ public class BoardService {
 	@Transactional
 	public void 글삭제하기(int id) {
 		boardrepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void 글수정하기(int id, Board requestBoard) {
+		Board board = boardrepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 찾기 실패 : 아이디를 찾을 수 없습니다."); 
+				}); // 영속화 완료
+		board.setTitle(requestBoard.getTitle());
+		board.setContent(requestBoard.getContent());
+		// 해당 함수가 종료시(Service 가 종료될 때) 트랜잭션이 종료됩니다. 이때 더티체킹  자동으로 업데이트가 됨. DB  
 	}
 }
