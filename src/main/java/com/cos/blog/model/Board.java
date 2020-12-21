15c36aed95acf84118f87573c5cc95a7bbb3ee8b
wkdrudgh <boo04178@naver.com>
2020-12-21 16:45:04 +0900

    댓글 목록 뿌리기

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -16,6 +16,8 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -46,7 +48,8 @@ public class Board {
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
 	
 	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 컬럼을 만들지 마세요.
-	private List<Reply> reply;
+	@JsonIgnoreProperties({"board"})
+	private List<Reply> replys;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
